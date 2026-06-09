41dc6c56a0f8411d03cda7bd9e22d2d086c8f176
Kareemgomaa <kareem.gomaa.2001@gmail.com>
2026-06-09 10:55:58 +0300

    add socket i.o

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+import http from "http";
 import app from "./app";
 import { env } from "./common/config/env";
 import { initContainer } from "./container/container";
+import { SocketServer } from "./modules/tracking/infrastructure/socket/socket.server";
 
 const PORT = env.PORT;
 
 initContainer();
 
-app.listen(PORT, () => {
-  console.log(`Server is running in ${env.NODE_ENV} mode on port ${PORT}`);
+const server = http.createServer(app);
+
+SocketServer.init(server);
+
+server.listen(PORT, () => {
+  console.log(
+    `Server & Socket.io are running in ${env.NODE_ENV} mode on port ${PORT}`,
+  );
 });
---
+++ src/modules/tracking/infrastructure/persistence/socket-location-stream.repository.ts
@@ -0,0 +1,33 @@
+import { injectable } from "tsyringe";
+import { ILocationStreamRepository } from "../../domain/repositories/i-location-stream.repository";
+import { LocationEntity } from "../../domain/entities/location.entity";
+import { SocketServer } from "../socket/socket.server";
+
+@injectable()
+export class SocketLocationStreamRepository implements ILocationStreamRepository {
+  async saveLiveLocation(location: LocationEntity): Promise<void> {
+    const livePayload = {
+      deviceId: location.deviceId,
+      userId: location.userId,
+      lat: location.coordinate.lat,
+      lng: location.coordinate.lng,
+      speed: location.speed,
+      address: location.address,
+      updatedAt: location.updatedAt,
+    };
+
+    console.log(
+      `[Socket Stream] Broadcasting live location for device: ${location.deviceId}`,
+    );
+
+    SocketServer.emitToRoom(
+      `device:${location.deviceId}`,
+      "location-updated",
+      livePayload,
+    );
+  }
+
+  async getLiveLocation(userId: string): Promise<LocationEntity | null> {
+    return null;
+  }
+}
